29f5fee6e01bc35da77687056bf7a91a92b81554
Baur Zhan <baur007@gmail.com>
2022-05-16 20:40:54 +0100

    testing task 3 GET /api/topics

--- __tests__/get-topics.test.js
+++ __tests__/get-topics.test.js
@@ -1,7 +1,32 @@
-const supertest = require('supertest');
+const request = require('supertest');
+const app = require('../app.js');
+const seed = require('../db/seeds/seed.js');
+const testData = require('../db/data/test-data/index.js');
+const db = require('../db/connection.js');
 
-describe('GET: /api/topics', () => {
-  test('test 1', () => {
-    
-  })
-})
+// afterAll( () => {
+//   db.end();
+// });
+
+beforeEach(() => {
+  seed(testData);
+});
+
+describe('GET /api/topics', () => {
+
+  test('responds with object containing all items', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveLength(3);
+        response.body.forEach(topic => {
+          expect(topic).toHaveProperty('slug');
+          expect(topic).toHaveProperty('description');
+        });
+      })
+      .catch((error) => {
+        if (error) throw error;
+      });
+  });
+});
