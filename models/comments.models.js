e11a44d8f909fdfdf90aeb4652af652ef1c32f9d
Baur Zhan <baur007@gmail.com>
2022-05-20 20:59:12 +0100

    forgot to add last version of comments.models.js

--- models/comments.models.js
+++ models/comments.models.js
@@ -5,20 +5,31 @@ exports.selectCommentsByArticleId = (articleId) => {
   return db.query(sqlQuery, [articleId])
   .then(({ rows: commentsRows }) => {
     if (commentsRows.length > 0) return commentsRows;
-    return Promise.reject({ code: 404, msg: 'Not found' });
+    return Promise.reject({ code: 'articleNotFound' });
   });
 };
 
 exports.insertCommentByArticleId = (articleId, comment) => {
-  // const dateObject = new Date(Date.now());
-  const sqlQuery = 'INSERT INTO comments (body, article_id, author) VALUES ($1, $2, $3) RETURNING *';
-  return db.query(sqlQuery, [comment.body, +articleId, comment.username])
-  .then(( {rows: insertedComment} ) => {
-    if (insertedComment.length > 0) {
-      // insertedComment[0].created_at = insertedComment[0].created_at.getTime();
-      console.log(insertedComment[0]);
-      return insertedComment[0];
-    };
-    return Promise.reject({ code: 404, msg: 'Not found' });
-  })
+  if (isNaN(articleId)) {
+    return Promise.reject({ code: 'articleIdisNaN'});
+  } 
+
+  if (Object.keys(comment).includes('username') && Object.keys(comment).includes('body')) {
+    const sqlQuery = 'INSERT INTO comments (body, article_id, author) VALUES ($1, $2, $3) RETURNING *';
+    return db.query(sqlQuery, [comment.body, articleId, comment.username])
+    .then(( {rows: insertedComment} ) => {
+      if (insertedComment.length > 0) {
+        return insertedComment[0];
+      };
+    return Promise.reject({ code: 'articleNotFound'});
+    })
+    .catch((error) => {
+      if (error.code = '23503') {
+        if (error.detail.startsWith('Key (article_id)')) return Promise.reject({ code: 'articleNotFound'});
+        if (error.detail.startsWith('Key (author)')) return Promise.reject({ code: 'authorNotFound'});
+      };
+    })
+  } else {
+    return Promise.reject({ code: 'missingProperty'});
+  };
 };
